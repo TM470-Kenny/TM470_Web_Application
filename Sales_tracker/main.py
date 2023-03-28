63831ea6face1152d2da8b331d2cd360413c619b
TM470-Kenny <zw349144@ou.ac.uk>
2023-03-28 20:32:45 +0100

    Shared page design created and sales tracker page started
    Shared design added across all pages including title, header, logout, and navigation bar. Created the layout for the sales tracker page ready to add in the form elements.

--- Sales_tracker/main.py
+++ Sales_tracker/main.py
@@ -3,5 +3,36 @@ from flask import Flask, render_template
 # instantiate the Flask object
 app = Flask(__name__)
 
+
+# basic route for login page - linking to html file
+@app.route('/')
+def login():
+    return render_template("login.html")
+
+
+# route for users page
+@app.route('/users/')
+def users():
+    return render_template("users.html")
+
+
+# route for database page
+@app.route('/database/')
+def database():
+    return render_template("database.html")
+
+
+# route for targets page
+@app.route('/targets/')
+def targets():
+    return render_template("targets.html")
+
+
+# route for sales tracker page
+@app.route('/sales/')
+def sales():
+    return render_template("sales.html")
+
+
 if __name__ == '__main__':
     app.run()
