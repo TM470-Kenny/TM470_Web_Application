f8a86027c216b6b475d9ba2319e086408136f128
TM470-Kenny <zw349144@ou.ac.uk>
2023-04-01 11:46:07 +0100

    Basis of sales tracker form created
    The form elements have been added for the sales tracker with placeholder values. Used Bootstrap in WTForms to style form. This will be updated to read from the database.

--- Sales_tracker/main.py
+++ Sales_tracker/main.py
@@ -1,7 +1,35 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, RadioField, IntegerField
+from wtforms.validators import DataRequired, NumberRange
+from flask_bootstrap import Bootstrap5
 
 # instantiate the Flask object
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+# instntiate bootstrap to be used with form
+bootstrap = Bootstrap5(app)
+
+
+# form design for sales tracker form
+class SalesForm(FlaskForm):
+    # set as current logged in user - admin can change
+    username = StringField('Username:', validators=[DataRequired()])
+    new_up = RadioField('New or Upgrade:', choices=['New', 'Upgrade'])
+    sale_type = SelectField('Sale Type:', choices=['Device', 'Sim', 'Broadband'])
+    # visible upon choosing device
+    device_name = StringField('Device Name:')
+    data_amount = SelectField('Data Amount:', choices=['1GB', '5GB', '100GB', 'Unlimited'])
+    # price dependent on data and device selected
+    price = SelectField('Price:', choices=['£10.00', '£14.00', '£20.00', '£35.00'])
+    discount = IntegerField('Discount:', validators=[NumberRange(min=0, max=100)])
+    # visible upon choosing device
+    insurance = SelectField('Insurance:', choices=['None', 'Tier 1 Damage', 'Tier 1 Full', 'Tier 2 Damage'
+                            , 'Tier 2 Full'])
+    # visible upon choosing broadband
+    broadband = SelectField('Broadband Type:', choices=['Copper', 'Fibre 1', 'Fibre 2', 'Full Fibre'])
+    submit = SubmitField('Submit')
 
 
 # basic route for login page - linking to html file
@@ -29,9 +57,10 @@ def targets():
 
 
 # route for sales tracker page
-@app.route('/sales/')
+@app.route('/sales/', methods=['GET', 'POST'])
 def sales():
-    return render_template("sales.html")
+    sales_form = SalesForm()
+    return render_template("sales.html", sales_form=sales_form)
 
 
 if __name__ == '__main__':
