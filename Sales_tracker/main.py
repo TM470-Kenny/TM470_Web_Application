f701869054ec27808ab8ecddd343be4f1e6d7173
TM470-Kenny <zw349144@ou.ac.uk>
2023-04-03 18:36:19 +0100

    Changed to a more reusable way of setting up database
    Removed the previous versions of setting up my database manually. Added in forms script to hold all the forms to be added. Created database using SQLAlchemy with db_setup script for the initial database setup. Added the products form and ability to add new products to the database using the form

--- Sales_tracker/main.py
+++ Sales_tracker/main.py
@@ -1,45 +1,110 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, RadioField, IntegerField
-from wtforms.validators import DataRequired, NumberRange
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 
-import sqlite3
+from forms import SalesForm, ProductsForm
+
+import os
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 # instantiate the Flask object
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
-# create connection to the database
-def connect_db():
-    conn = sqlite3.connect('sales_tracker.db')
-    conn.row_factory = sqlite3.Row
-    return conn
 
+# finds the full path for the current directory of this file
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# connect flask app to database
+# set up database location
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'sales_tracker.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db = SQLAlchemy(app)
+
+
+#####################################
+class Products(db.Model):
+
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    device = db.Column(db.Text)
+    data = db.Column(db.Integer)
+    length = db.Column(db.Integer)
+    price = db.Column(db.Numeric)
+    revenue = db.Column(db.Numeric)
+    commission = db.Column(db.Numeric)
+    # Set up One to Many relationship
+    product_sales = db.relationship('Sales', backref='product', lazy='dynamic')
+
+    def __init__(self,device,data,length,price,revenue,commission):
+        self.device = device
+        self.data = data
+        self.length = length
+        self.price = price
+        self.revenue = revenue
+        self.commission = commission
+
+    def __repr__(self):
+        return f'{self.device} with {self.data} costs {self.price}'
+
+    def report_sales(self):
+        for sale in self.product_sales:
+            print(sale)
+
+
+class Sales(db.Model):
+
+    __tablename__ = 'sales'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('users.id'))
+    new = db.Column(db.Boolean)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    discount = db.Column(db.Integer)
+    insurance = db.Column(db.Boolean)
+
+    def __init__(self,user,new,product_id,discount,insurance):
+        self.user = user
+        self.new = new
+        self.product_id = product_id
+        self.discount = discount
+        self.insurance = insurance
+
+    def __repr__(self):
+        return f'{self.user} sold product id {self.product_id} with {self.discount}% discount'
+
+class Users(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text)
+    firstname = db.Column(db.Text)
+    lastname = db.Column(db.Text)
+    password = db.Column(db.Text)
+
+    user_sales = db.relationship('Sales', backref='users', lazy='dynamic')
+
+    def __init__(self,username,firstname,lastname,password):
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
+        self.password = password
+
+    def __repr__(self):
+        return f'{self.firstname} {self.lastname} has username: {self.username}'
+
+
+
+######################################
 
 # instantiate bootstrap to be used with form
 bootstrap = Bootstrap5(app)
 
-# form design for sales tracker form
-class SalesForm(FlaskForm):
-    # set as current logged in user - admin can change
-    username = StringField('Username:', validators=[DataRequired()])
-    new_up = RadioField('New or Upgrade:', choices=['New', 'Upgrade'])
-    sale_type = SelectField('Sale Type:', choices=['Device', 'Sim', 'Broadband'])
-    # visible upon choosing device
-    device_name = StringField('Device Name:')
-    data_amount = SelectField('Data Amount:', choices=['1GB', '5GB', '100GB', 'Unlimited'])
-    contract_length = SelectField('Contract Length:', choices=['1m', '12m', '24m'])
-    # price dependent on data and device selected
-    price = SelectField('Price:', choices=['£10.00', '£14.00', '£20.00', '£35.00'])
-    discount = IntegerField('Discount:', validators=[NumberRange(min=0, max=100)])
-    # visible upon choosing device
-    insurance = SelectField('Insurance:', choices=['None', 'Tier 1 Damage', 'Tier 1 Full', 'Tier 2 Damage'
-                            , 'Tier 2 Full'])
-    # visible upon choosing broadband
-    broadband = SelectField('Broadband Type:', choices=['Copper', 'Fibre 1', 'Fibre 2', 'Full Fibre'])
-    submit = SubmitField('Submit')
+
 
 
 # basic route for login page - linking to html file
@@ -55,13 +120,23 @@ def users():
 
 
 # route for database page
-@app.route('/database/')
+@app.route('/database/', methods=['GET', 'POST'])
 def database():
-    # connect to database and read all elements for products table
-    conn = connect_db()
-    products = conn.execute('SELECT * FROM products').fetchall()
-    conn.close()
-    return render_template("database.html", products=products)
+    products_form = ProductsForm()
+    all_products = Products.query.all()
+    if products_form.validate_on_submit():
+        device = products_form.device_name.data
+        data = products_form.data_amount.data
+        length = products_form.contract_length.data
+        price = products_form.price.data
+        revenue = products_form.revenue.data
+        commission = products_form.commission.data
+
+        new_product = Products(device, data, length, price, revenue, commission)
+        db.session.add(new_product)
+        db.session.commit()
+        return redirect(url_for("database"))
+    return render_template("database.html", products_form=products_form, all_products=all_products)
 
 
 # route for targets page
@@ -74,13 +149,9 @@ def targets():
 @app.route('/sales/', methods=['GET', 'POST'])
 def sales():
     sales_form = SalesForm()
-    # connect to database and read all elements for sales table
-    conn = connect_db()
-    all_sales = conn.execute('SELECT * FROM sales').fetchall()
-    conn.close()
-    return render_template("sales.html", sales_form=sales_form, all_sales=all_sales)
+    return render_template("sales.html", sales_form=sales_form)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
